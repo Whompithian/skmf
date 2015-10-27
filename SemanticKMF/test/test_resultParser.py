b2cceb13f03343c8b8fa775c9d12238013386d2a
Brendan Sweeney <bps7@uw.edu>
2015-10-26 22:12:15 -0700

    Further test case layout.

---
+++ SemanticKMF/test/test_resultParser.py
@@ -0,0 +1,10 @@
+import unittest
+
+class SemanticKmfTest(unittest.TestCase):
+
+    def test_backend_https_connect(self):
+        self.assertEqual(4, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
